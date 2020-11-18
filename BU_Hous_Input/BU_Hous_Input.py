2f9e9eec435121792e6c3a8186e08a7cd5504506
Kradon <kradonzhao@gmail.com>
2020-11-18 14:00:40 -0500

    access studentlink
    The driver will now navigate to the studentlink work tab page.
    Added parathensis to driver intialization to stop errors.

--- BU_Hous_Input/BU_Hous_Input.py
+++ BU_Hous_Input/BU_Hous_Input.py
@@ -2,4 +2,5 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox()
+driver.get("https://www.bu.edu/link/bin/uiscgi_studentlink.pl/1605725845?ModuleName=menu.pl&NewMenu=Work")
 driver.close()
