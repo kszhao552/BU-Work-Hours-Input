cbc1abf952bd554eae8f81e36533f85d7de1fe1d
Kradon <kradonzhao@gmail.com>
2020-11-18 15:57:23 -0500

    create function to log in
    Function will access the site and log in.
    Right now, it access the site. Needs to get Username and Password from a file.

--- BU_Hous_Input/BU_Hous_Input.py
+++ BU_Hous_Input/BU_Hous_Input.py
@@ -1,6 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
+
+
+def login(driver):
+    driver.get("https://www.bu.edu/link/bin/uiscgi_studentlink.pl/1605725857?ModuleName=time_entry/job_summary.pl")
+    element = driver.find_element_by_id("j_username")
+    element.send_keys("Username")
+
 
 driver = webdriver.Firefox()
-driver.get("https://www.bu.edu/link/bin/uiscgi_studentlink.pl/1605725845?ModuleName=menu.pl&NewMenu=Work")
+login(driver)
+time.sleep(3)
 driver.close()
