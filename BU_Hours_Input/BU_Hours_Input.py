c0b9297501f81d14062eaa499e58e4151e601617
Kradon <kradonzhao@gmail.com>
2020-11-20 21:56:15 -0500

    fix: log in properly
    PRogram would not click the button to push the notification before.
    The driver checks before returning.
    Added timer to detect a time out for duo.
    TODO: go into the page and input the time.

--- BU_Hours_Input/BU_Hours_Input.py
+++ BU_Hours_Input/BU_Hours_Input.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import config
-
+import sys
 
 def login(driver):
     driver.get("https://www.bu.edu/link/bin/uiscgi_studentlink.pl/1605725857?ModuleName=time_entry/job_summary.pl")
@@ -12,20 +15,24 @@ def login(driver):
     element.send_keys(config.password)
     element.send_keys(Keys.ENTER)
     
-    driver.implicitly_wait(3)
+    driver.implicitly_wait(20)
     window_after = driver.window_handles[0]
     driver.switch_to.window(window_after)
     
-    driver.switch_to.frame('duo_iframe')#/div/div/div/div/form/div/fieldset/div[@class = "row-label push-label"]')#update to get xpath inside div
-    element = driver.find_element_by_xpath('//div/div/div/div/form/div/fieldset/div/button')
-    print(element)
-    try:
-        element.click()
-        driver.implicitly_wait(60)
-        print("2FA failed. Please run program again.")
-    except:
-        return
-
+    driver.switch_to.frame('duo_iframe')
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div/div/div/div/form/div/fieldset/div/button')))
+    driver.find_element_by_xpath('//div/div/div/div/form/div/fieldset/div/button').click()
+    element.click()
+    start = time.time()
+    while True:
+        if "Login" not in driver.title:
+            return
+        elif(time.time() - start >= 60):
+            print("2FA failed, please try again")
+            driver.close()
+            sys.exit()
+    
+    
 
 driver = webdriver.Firefox()
 login(driver)
